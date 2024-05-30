0f97c537831dff0961ed2c71307bca02c01f7f47
alexsandroferreira <ferreira.alex@outlook.com>
2024-05-29 22:08:13 -0300

    feat: finished sidebar component

---
+++ src/app/components/Sidebar/Profile.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+import { LogOut } from 'lucide-react'
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        src="https://github.com/alexsandroferreira.png"
+        alt=""
+        className="h-10 w-10 rounded-full"
+      />
+
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Alex Ferreira
+        </span>
+        <span className=" truncate text-sm text-zinc-500">
+          alexFerreira@gmail.com
+        </span>
+      </div>
+
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-100"
+      >
+        <LogOut className=" h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
---
+++ src/app/components/input.tsx
@@ -0,0 +1,35 @@
+import React, { type ComponentProps } from 'react'
+
+interface InputPrefixProps extends ComponentProps<'div'> {}
+
+export function InputPrefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+interface InputControlProps extends ComponentProps<'input'> {}
+
+export function InputControl(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+
+interface InputRootProps extends ComponentProps<'div'> {}
+
+export function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      {...props}
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+    />
+  )
+}
+
+export const Input = {
+  Prefix: InputPrefix,
+  Control: InputControl,
+  Root: InputRoot,
+}
