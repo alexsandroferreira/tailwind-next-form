517d5aefef297717795f8140417aca14ab3300a9
alexsandroferreira <ferreira.alex@outlook.com>
2024-06-20 17:13:09 -0300

    feat: add filelist component

--- src/app/components/Form/FileInput/Root.tsx
+++ src/app/components/Form/FileInput/Root.tsx
@@ -12,7 +12,7 @@ export type RootPros = ComponentProps<'div'>
 type FileInputContextType = {
   id: string
   files: File[]
-  onFilesSelected: (file: File[]) => void
+  onFilesSelected: (file: File[], multiple: boolean) => void
 }
 
 const FileInputContext = createContext({} as FileInputContextType)
@@ -21,8 +21,16 @@ export function Root(props: RootPros) {
   const id = useId()
   const [files, setFiles] = useState<File[]>([])
 
+  function onFilesSelected(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((state) => [...state, ...files])
+    } else {
+      setFiles(files)
+    }
+  }
+
   return (
-    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
+    <FileInputContext.Provider value={{ id, files, onFilesSelected }}>
       <div {...props} />
     </FileInputContext.Provider>
   )
