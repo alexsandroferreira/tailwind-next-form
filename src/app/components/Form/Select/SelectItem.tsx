dd46a489892cb8ec3b4a44578946904147c3e79e
alexsandroferreira <ferreira.alex@outlook.com>
2024-06-25 15:46:02 -0300

    feat: fineshed dark mode forms

--- src/app/components/Form/Select/SelectItem.tsx
+++ src/app/components/Form/Select/SelectItem.tsx
@@ -13,11 +13,14 @@ export function SelectItem({ text, ...props }: SelectItemProps) {
     px-3
     py-2.5
     outline-none
-  data-[highlighted]:bg-zinc-50"
+  data-[highlighted]:bg-zinc-50
+  dark:data-[highlighted]:bg-zinc-600"
       {...props}
     >
-      <Select.ItemText className="text-black">{text}</Select.ItemText>
-      <Select.ItemIndicator className="h4 w-4 text-violet-500">
+      <Select.ItemText asChild>
+        <span className="text-black dark:text-zinc-100">{text}</span>
+      </Select.ItemText>
+      <Select.ItemIndicator className="h4 w-4 text-violet-500 dark:text-violet-300">
         <Check className="h-4 w-4" />
       </Select.ItemIndicator>
     </Select.Item>
--- src/app/components/Form/Textarea/index.tsx
+++ src/app/components/Form/Textarea/index.tsx
@@ -6,15 +6,12 @@ export function Textarea(props: TextareaProps) {
   return (
     <textarea
       className="
-        min-h-[120px]
-        w-full resize-y 
-        rounded-lg border 
-        border-zinc-300 px-3 
-        py-2 shadow-sm 
-        outline-none
-      focus:border-violet-300
-        focus:ring-2
-      focus:ring-violet-100"
+        min-h-[120px] w-full resize-y rounded-lg border 
+      border-zinc-300 px-3 py-2 shadow-sm outline-none
+      focus:border-violet-300 focus:ring-2 focus:ring-violet-100
+      dark:border-r-zinc-900 dark:bg-zinc-800 dark:text-zinc-100 dark:placeholder-zinc-500
+      dark:focus:border-violet-500 dark:focus:ring-violet-500/20
+      "
       {...props}
     />
   )
