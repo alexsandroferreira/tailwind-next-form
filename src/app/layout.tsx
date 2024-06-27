011761dfb192f10bc4d2f2c9fa658d62382f6908
alexsandroferreira <ferreira.alex@outlook.com>
2024-06-26 21:32:48 -0300

    feat: add toggle dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import { Sidebar } from './components/Sidebar'
+import Toggle from './components/Toggle/index'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className=" antialiased">
       <body className={inter.className}>
-        <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
+        <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app ">
           <Sidebar />
+          <Toggle className="fixed bottom-4 right-10 z-10 cursor-pointer lg:absolute lg:right-8 lg:top-8 lg:z-50" />
           <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
             {children}
           </main>
